ba3670eabd2597c89d33ae50c3e60474f1c2c37c
linzhuofei <philip.f.lin@gmail.com>
2024-10-18 15:50:55 +0800

    分布式鎖 - Redisson 的 multiLock 原理

--- src/test/java/com/hmdp/RedissonTest.java
+++ src/test/java/com/hmdp/RedissonTest.java
@@ -8,6 +8,7 @@ import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @SpringBootTest
@@ -16,17 +17,28 @@ class RedissonTest {
     @Resource
     private RedissonClient redissonClient;
 
+    @Resource
+    private RedissonClient redissonClient2;
+
+    @Resource
+    private RedissonClient redissonClient3;
+
     private RLock lock;
 
     @BeforeEach
     void setUp(){
-        lock = redissonClient.getLock("order");
+        RLock lock1 = redissonClient.getLock("order");
+        RLock lock2 = redissonClient2.getLock("order");
+        RLock lock3 = redissonClient3.getLock("order");
+
+        // 創建連鎖 multiLock
+        lock = redissonClient.getMultiLock(lock1, lock2, lock3);
     }
 
     @Test
-    void method1(){
+    void method1() throws InterruptedException {
         // 嘗試獲取鎖
-        boolean isLock = lock.tryLock();
+        boolean isLock = lock.tryLock(1L, TimeUnit.MINUTES);
         if (!isLock){
             log.error("獲取鎖失敗 ... 1");
             return;
